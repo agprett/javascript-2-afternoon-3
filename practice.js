40d186ab89866fcc51bd46120faf0102e5879761
Alec Prettyman <alecprettyman@Alecs-MacBook-Pro.local>
2020-03-05 16:33:49 -0700

    completed practice

--- practice.js
+++ practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first(array, cb){
+  cb(array[0])
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +51,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(array, cb){
+  cb(array[array.length - 1])
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,6 +72,9 @@ last(names, function(lastName){
 */
 
 //Code Here
+function multiply(num1, num2, cb){
+  cb(num1 * num2)
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +94,15 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(array, name, cb){
+  for(i = 0; i <= array.length - 1; i++){
+    if(array[i] === name){
+      cb(true)
+    } else {
+      cb(false)
+    }
+  }
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,6 +124,16 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq(array, cb){
+  for(i = 0; i <= array.length - 1; i++){  
+    for(k = i + 1; k <= array.length - 1; k++){
+      if(array[i] === array[k]){
+        array.splice(k, 1)
+      }
+    }
+  }
+  cb(array)
+}
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -123,6 +151,11 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+function each(array, cb){
+  for(let i = 0; i <= array.length - 1; i++){
+    cb(array[i], i)
+  }
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -140,6 +173,13 @@ each(names, function(item, indice){
 */
 
 // Code here
+function getUserById(users, id, cb){
+  for(let i = 0; i < users.length; i ++){
+    if(users[i].id === id){
+      cb(users[i])
+    }
+  }
+}
 
 // Do not edit the code below.
 var users = [
